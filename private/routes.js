674f273fd27eb5066a95ceb310927d0a9efed574
Henry Aldous <aldous1214@gmail.com>
2021-04-26 11:16:38 -0500

    functional app. File paths trouble

---
+++ private/routes.js
@@ -0,0 +1,54 @@
+var path = require("path");
+var fs = require("fs");
+let noteData = require('./develop/db/db.json');
+
+module.exports = function (app) {
+    // API ROUTES
+    app.get("/api/notes", (req, res) => {
+        fs.readFile("./develop/db/db.json", (err, data) => {
+            if (err) throw err;
+            return res.json(JSON.parse(data));
+        });
+    });
+
+    app.post("/api/notes", (req, res) => {
+        let inputtedData = req.body;
+
+        // Add a unique id to the note
+        let uid = genRandomNum(1, 9999);
+        inputtedData["id"] = uid;
+
+        noteData.push(inputtedData);
+        fs.writeFile('./develop/db/db.json', JSON.stringify(noteData), (err) => {
+            if (err) throw err;
+            return res.json(inputtedData);
+        });
+    });
+
+    app.delete("/api/notes/:id", (req, res) => {
+        let deletionTarget = req.params.id;
+        for (i = 0; i < noteData.length; i++) {
+            if (deletionTarget == noteData[i].id) {
+                noteData.splice(i, 1);
+            }
+        }
+        fs.writeFile("./develop/db/db.json", JSON.stringify(noteData), (err) => {
+            if (err) throw err;
+            return;
+        });
+        res.end();
+    });
+    
+    // HTML ROUTES
+    app.get("/notes", function (req, res) {
+        res.sendFile(path.join(__dirname, "./develop/public/notes.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "./develop/public/index.html"));
+    });
+};
+
+function genRandomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
