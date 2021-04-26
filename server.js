70459bf0275737b84af9cfac301612f0e5aa2bdc
Henry Aldous <aldous1214@gmail.com>
2021-04-26 16:13:34 -0500

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var path = require("path");
 
 var app = express();
-var PORT = process.env.PORT || 3500;
+var PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
